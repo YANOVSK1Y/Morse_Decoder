1df183a273b0d3f8b30bdd46cc4df07c3b3883f7
YANOVSK1Y <andreyianovskyi78@gmail.com>
2021-09-07 13:01:05 +0300

    Finish morse encode to text

--- main.py
+++ main.py
@@ -40,24 +40,48 @@ class Morse():
         '8':'11100',
         '9':'11110',
         '0':'11111',
+        #symbols
+        ',':'110011',
+        '?':'001100',
+        '-':'100001',
+        '.':'010101'
+
         }
 
 
     def toMorse(self, message):
+        res = ''
         res_lst = list()
         for i in str(message):
             if i == ' ' or i == '  ':
                 res_lst.append(i)
             elif str(i.upper()) in self.morse_code.keys():
                 res_lst.append(self.morse_code.get(str(i.upper())))
+                res_lst.append('|')
             else:
-                res_lst.append('~')
-        
-        return res_lst
-
+                pass
+        for i in res_lst:
+            res += str(i)
+        return res
 
     def toText(self, code):
-        pass
+        res = ''
+        item = ''
+        for i in code.split('|'):
+            if len(i.split(' ')) == 2:
+                res += ' '
+                for key, value in self.morse_code.items():
+                    if value == i.split(' ')[1]:
+                        res += key
+            elif len(i.split(' ')) == 1:
+                for key, value in self.morse_code.items():
+                    if value == i:
+                        res += key
+            else:
+                pass
+
+
+        return res
 
 
     def _logic(self):
@@ -71,18 +95,16 @@ class Morse():
             message = str(input())
             result = self.toMorse(message)
         elif user_choise == '2':
-            pass
+            print('Enter code to converting:')
+            code_message = str(input())
+            result = self.toText(code_message)
         else:
             print('Ooops, no such command as' + str(user_choise))
         return result
 
 
     def main(self):
-        result = ''
-        result_list = self._logic()
-        for i in result_list:
-            result += i
-        return result
+        return self._logic()
 
 
 if __name__ == '__main__':
